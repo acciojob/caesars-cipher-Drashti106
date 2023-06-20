491b5866454f75aa1984a65708dda59a2a73c17a
Drashti Doshi <118449527+Drashti106@users.noreply.github.com>
2023-06-20 21:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function rot13(encodedStr) {
 let decodedStr = "";
 	for(let i = 0;i<encodedStr.length;i++){
 		if(lookup[encodedStr[i]] === undefined)
-		decodedStr += encodedStrstr[i];
+		decodedStr += encodedStr[i];
 		else
 		decodedStr += lookup[encodedStr[i]];
 	}
